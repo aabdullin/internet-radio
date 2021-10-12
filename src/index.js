a4b535372313ddf4019a02a9efffe66796a9478c
Artur <rutrika@gmail.com>
2021-10-12 16:18:38 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -1,52 +1,53 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 import './index.css'
 import 'font-awesome/css/font-awesome.min.css';
 
+const Page = () => ( 
+    <div>
+        <CountingParent/>
+        <CountingParent/>
+        <CountingParent/>
+    </div>
+)
 
-function Title ( {testFile} ) {
-    return (
-        <div className="genre">
-            {testFile}
+
+class CountingParent extends React.Component { // The constructor is called when a
+    // component is created
+    constructor(props) {
+        super(props);
+            // Set the state here. Use "props" if needed.
+        this.state = { 
+            actionCount: 0
+        };
+        // Bind the event handler function, so that its // `this` binding isn't lost when it gets passed // to the button
+        this.handleAction = this.handleAction.bind(this);
+    }
+    handleAction(action) {
+        console.log('Child says', action);
+        // Replace actionCount with an incremented value this.setState({
+        this.setState = this.state.actionCount + 1 
+    };
+
+    render() { return (
+        <div>
+            <Child onAction={this.handleAction}/> <p>Clicked {this.state.actionCount} times</p>
         </div>
-    )
-}
+        ); 
+    }
+}    
 
-Title.propTypes = {
-    card: PropTypes.object.isRequired
-}
 
-function RadioStationList ( {testFiles} ) {
+function Child({ onAction }) { 
     return (
-        <div className="internet_radio">
-                <div className="title"> </div>
-                <div className="list_of_stations">
-                    {testFiles.map ( (genre, index) => {
-                        console.log (genre, index)
-                        let size = 'btn-xs';
-                        if (index < 10) {
-                          size = 'btn-lg'
-                        } else if (index < 30) {
-                          size = 'btn-md'
-                        }
-                        return <button className={`${size}`} key={index}> {genre} </button>
-                })}
-                </div>
-        </div>
-    )
-}
-                 
-RadioStationList.propTypes = {
-    testFiles: PropTypes.array
-}
-
-
-const testGenres = ["Jazz", "Top 40", "Country", "Blues", "Easy Listening", "Rock", "Classical", "80s", "Chillout", "Oldies", "Dance", "Trance", "Reggae", "Ambient", "Hip Hop", "Smooth Jazz", "70s", "House", "Lounge", "Metal", "Drum And Bass", "Meditation", "Techno", "Heavy Metal", "Soul", "60s", "Pop", "Psytrance", "Bollywood", "90s", "Latin", "50s", "Funk", "Rap", "Rockabilly", "Hindi", "Greek", "Minimal", "Comedy", "Alternative", "Bluegrass", "New Age", "Edm", "Manele", "Reggaeton", "Salsa", "Swing", "Disco", "Japanese", "Classic Rock", "Chill", "Electronic", "Dancehall", "Talk", "Rnb", "Psychedelic", "Dub", "Dubstep", "Progressive", "Hardstyle", "Romantic", "Indie", "Goa", "Eurodance", "Kizomba", "Folk", "Hardcore", "Celtic", "Americana", "New Wave", "Goth", "Electro", "Soundtracks", "Jungle", "Jewish", "Opera", "Indian", "Punk", "Downtempo", "Garage"];
+    <button onClick={onAction}> 
+        Click Me!
+    </button>
+); }
 
 
 ReactDOM.render( 
-    <RadioStationList testFiles={testGenres}/>, 
+    <Page/>, 
     document.querySelector('#root')
 )
 
